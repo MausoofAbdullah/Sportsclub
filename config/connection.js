b740c96ff4b79c2c868978b682ab41fb837e3a99
MausoofAbdullah <mausoofabdullah@gmail.com>
2022-10-23 17:32:28 +0530

    db atlas connection

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const state = {
 
 
 module.exports.connect = function (done) {
-    const url = 'mongodb://localhost:27017'
-    const dbname = 'newSports'
+    const url = 'mongodb+srv://MausoofAbdullah2:9686327955@cluster0.79bwqcz.mongodb.net/?retryWrites=true&w=majority'
+    const dbname = 'SportsClub'
 
     mongoClient.connect(url, (err, data) => {
         if (err) return done(err)
