f83afca3f9c5d7d619d78f1f32a9f4accebfda33
MausoofAbdullah <mausoofabdullah@gmail.com>
2022-10-25 15:12:41 +0530

    hiding database connection

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,10 @@ const state = {
 
 
 module.exports.connect = function (done) {
-    const url = 'mongodb+srv://MausoofAbdullah2:9686327955@cluster0.79bwqcz.mongodb.net/?retryWrites=true&w=majority'
+    const url=process.env.DATABASE  
+    // const url = 'mongodb+srv://MausoofAbdullah2:9686327955@cluster0.79bwqcz.mongodb.net/?retryWrites=true&w=majority'
     const dbname = 'SportsClub'
+    process.env.DATABASE 
 
     mongoClient.connect(url, (err, data) => {
         if (err) return done(err)
